12fe705a0f485b0badc2533fc9627a3d70b254a9
Saner Turhaner <saner.turhaner@tum.de>
2018-06-10 20:19:56 +0200

    Dashboard Schedule Breadcrumbs is done

--- src/components/DashboardSchedule/Breadcrumbs.js
+++ src/components/DashboardSchedule/Breadcrumbs.js
@@ -3,12 +3,48 @@ import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ColorPalette from '../../constants/ColorPalette'
 
+const BreadcrumbsDiv = styled.div`    
+  font-size: 14px;
+  display: flex;
+  flex-flow: row nowrap;		
+  margin: 20px 10px 20px 10px;
+  font-weight: bold;
+  color: ${ ColorPalette.primaryDark };
+`;
+
+const NavLink = styled.a`
+  font-size: 14px;
+  margin: auto;
+  margin-left: 10px;
+  text-decoration: none;
+  color: ${ ColorPalette.primary };
+  
+  &:hover {
+    font-style: italic;
+    color: ${ ColorPalette.accent };
+	}	
+`;
+
+const Divider = styled.p`
+  font-size: 14px;
+  margin: auto;
+  margin-left: 10px;
+  text-decoration: none;
+  color: ${ ColorPalette.primary };
+`;
+
 class Breadcrumbs extends Component {
   render() {
     return (
-      <Router>
-
-      </Router>
+      <BreadcrumbsDiv>You are here:
+        <NavLink href="index.html">Home</NavLink>
+        <Divider>|</Divider>
+        <NavLink href="index.html">Profile</NavLink>
+        <Divider>|</Divider>
+        <NavLink href="index.html">Dashboard</NavLink>
+        <Divider>|</Divider>
+        <NavLink href="index.html">Manage Schedule</NavLink>
+      </BreadcrumbsDiv>
     );
   }
 }
