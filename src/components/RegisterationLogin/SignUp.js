8d760a691fb4f4d23b2df9a2691e71c2c3dc84d5
Saner Turhaner <saner.turhaner@tum.de>
2018-06-18 23:11:39 +0200

    SignUp styling is finished

--- src/components/RegisterationLogin/SignUp.js
+++ src/components/RegisterationLogin/SignUp.js
@@ -1,14 +1,84 @@
 import React, { Component } from 'react';
+import user_icon from '../../resources/user-icon.png';
+import email_icon from '../../resources/email-icon.png';
+import password_icon from '../../resources/pass-icon.png';
 import {  Link,  withRouter } from 'react-router-dom';
 import { auth, db } from '../Firebase';
 import styled from "styled-components";
+import ColorPalette from "../../constants/ColorPalette";
 
 const ContentDiv = styled.div`
-  width: 600px;
-  display: flex column;
+  margin: 20px auto;
+  width: 450px; 
+  -webkit-border-radius: 8px/7px; 
+  -moz-border-radius: 8px/7px; 
+  border-radius: 8px/7px; 
+  background-color: #ebebeb; 
+  -webkit-box-shadow: 1px 2px 5px rgba(0,0,0,.31); 
+  -moz-box-shadow: 1px 2px 5px rgba(0,0,0,.31); 
+  box-shadow: 1px 2px 5px rgba(0,0,0,.31); 
+  border: solid 1px #cbc9c9;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5em;
+  font-weight: 300;
+  color: #4c4c4c;
+  text-align: center;
   margin: auto;
-  padding: 15px;
-  border: 1px solid black;
+  margin-top: 5%;
+  margin-bottom: 5%;
+`;
+
+const InputForm = styled.input`    
+  font-size: 1.2em;
+  outline: none; // Chrome outline
+  box-shadow: none; // Firefox outline
+  -webkit-appearance: none; // mobile Safari
+  background: transparent;
+  display: inline-block;
+  height: 2em;
+  width: 18em;
+  padding: 0 3%;
+  -webkit-border-radius: 0px 4px 4px 0px/5px 5px 4px 4px; 
+  -moz-border-radius: 0px 4px 4px 0px/0px 0px 4px 4px; 
+  border-radius: 0px 4px 4px 0px/5px 5px 4px 4px; 
+  background-color: #fff; 
+  -webkit-box-shadow: 1px 2px 5px rgba(0,0,0,.09); 
+  -moz-box-shadow: 1px 2px 5px rgba(0,0,0,.09); 
+  box-shadow: 1px 2px 5px rgba(0,0,0,.09); 
+  border: solid 1px #cbc9c9;
+`;
+
+const ButtonForm = styled.button`  
+  display: block;  
+	margin: auto;
+  margin-top: 5%;
+  margin-bottom: 5%;
+  height: 30px;
+  width: 100px;
+  font-size: 1em;
+  border-radius: 10px;
+  color: black;
+  
+  &:hover {
+    background-color: ${ ColorPalette.linksHover };
+	}	
+`;
+
+const IconForm = styled.img`  
+  display: inline-block;
+  float: left;
+  height: 2.2em;
+  width: 3.2em;
+`;
+
+const LineDiv = styled.div`  
+  display: block;
+	margin: auto;
+  margin-left: 50px;
+  margin-bottom: 5%;
+  margin-top: 5%;
 `;
 
 const Paragraph = styled.p`
@@ -20,7 +90,8 @@ const Paragraph = styled.p`
 
 const SignUpPage = ({ history }) =>
   <ContentDiv>
-    <h1>Sign Up</h1>
+    <Title>Sign Up</Title>
+    <hr/>
     <SignUpForm history={history} />
   </ContentDiv>
 
@@ -92,33 +163,46 @@ class SignUpForm extends Component {
 
     return (
       <form onSubmit={this.onSubmit}>
-        <input
-          value={username}
-          onChange={event => this.setState(updateByPropertyName('username', event.target.value))}
-          type="text"
-          placeholder="Full Name"
-        />
-        <input
-          value={email}
-          onChange={event => this.setState(updateByPropertyName('email', event.target.value))}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          value={passwordOne}
-          onChange={event => this.setState(updateByPropertyName('passwordOne', event.target.value))}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          value={passwordTwo}
-          onChange={event => this.setState(updateByPropertyName('passwordTwo', event.target.value))}
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <button disabled={isInvalid} type="submit">
+        <LineDiv>
+          <IconForm src={user_icon}/>
+          <InputForm
+            value={username}
+            onChange={event => this.setState(updateByPropertyName('username', event.target.value))}
+            type="text"
+            placeholder="Full Name"
+          />
+        </LineDiv>
+        <LineDiv>
+          <IconForm src={email_icon}/>
+          <InputForm
+            value={email}
+            onChange={event => this.setState(updateByPropertyName('email', event.target.value))}
+            type="text"
+            placeholder="Email Address"
+          />
+        </LineDiv>
+        <LineDiv>
+          <IconForm src={password_icon}/>
+          <InputForm
+            value={passwordOne}
+            onChange={event => this.setState(updateByPropertyName('passwordOne', event.target.value))}
+            type="password"
+            placeholder="Password"
+          />
+        </LineDiv>
+        <LineDiv>
+          <IconForm src={password_icon}/>
+          <InputForm
+            value={passwordTwo}
+            onChange={event => this.setState(updateByPropertyName('passwordTwo', event.target.value))}
+            type="password"
+            placeholder="Confirm Password"
+          />
+        </LineDiv>
+        <hr/>
+        <ButtonForm disabled={isInvalid} type="submit">
           Sign Up
-        </button>
+        </ButtonForm>
 
         { error && <p>{error.message}</p> }
       </form>
