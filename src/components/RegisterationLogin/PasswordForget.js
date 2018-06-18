508d3b662d3cdbf0d12f69d93081d59571e31762
Saner Turhaner <saner.turhaner@tum.de>
2018-06-18 23:05:54 +0200

    PasswordForget styling is finished

--- src/components/RegisterationLogin/PasswordForget.js
+++ src/components/RegisterationLogin/PasswordForget.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import email_icon from '../../resources/email-icon.png';
 import { Link } from 'react-router-dom';
 import { auth } from '../Firebase';
 import styled from "styled-components";
+import ColorPalette from "../../constants/ColorPalette";
 
 const ContentDiv = styled.div`
-  width: 600px;
-  display: flex column;
+  margin: 20px auto;
+  width: 450px; 
+  -webkit-border-radius: 8px/7px; 
+  -moz-border-radius: 8px/7px; 
+  border-radius: 8px/7px; 
+  background-color: #ebebeb; 
+  -webkit-box-shadow: 1px 2px 5px rgba(0,0,0,.31); 
+  -moz-box-shadow: 1px 2px 5px rgba(0,0,0,.31); 
+  box-shadow: 1px 2px 5px rgba(0,0,0,.31); 
+  border: solid 1px #cbc9c9;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5em;
+  font-weight: 300;
+  color: #4c4c4c;
+  text-align: center;
   margin: auto;
-  padding: 15px;
-  border: 1px solid black;
+  margin-top: 5%;
+  margin-bottom: 5%;
 `;
 
 const Paragraph = styled.p`
@@ -18,9 +35,61 @@ const Paragraph = styled.p`
   margin-top: 5%;
 `;
 
+const InputForm = styled.input`    
+  font-size: 1.2em;
+  outline: none; // Chrome outline
+  box-shadow: none; // Firefox outline
+  -webkit-appearance: none; // mobile Safari
+  background: transparent;
+  display: inline-block;
+  height: 2em;
+  width: 18em;
+  padding: 0 3%;
+  -webkit-border-radius: 0px 4px 4px 0px/5px 5px 4px 4px; 
+  -moz-border-radius: 0px 4px 4px 0px/0px 0px 4px 4px; 
+  border-radius: 0px 4px 4px 0px/5px 5px 4px 4px; 
+  background-color: #fff; 
+  -webkit-box-shadow: 1px 2px 5px rgba(0,0,0,.09); 
+  -moz-box-shadow: 1px 2px 5px rgba(0,0,0,.09); 
+  box-shadow: 1px 2px 5px rgba(0,0,0,.09); 
+  border: solid 1px #cbc9c9;
+`;
+
+const IconForm = styled.img`  
+  display: inline-block;
+  float: left;
+  height: 2.2em;
+  width: 3.2em;
+`;
+
+const ButtonForm = styled.button`  
+  display: block;  
+	margin: auto;
+  margin-top: 5%;
+  margin-bottom: 5%;
+  height: 30px;
+  width: 150px;
+  font-size: 1em;
+  border-radius: 10px;
+  color: black;
+  
+  &:hover {
+    background-color: ${ ColorPalette.linksHover };
+	}	
+`;
+
+const LineDiv = styled.div`  
+  display: block;
+	margin: auto;
+  margin-left: 50px;
+  margin-bottom: 5%;
+  margin-top: 5%;
+`;
+
 const PasswordForgetPage = () =>
   <ContentDiv>
-    <h1>Password Forget</h1>
+    <Title>Password Forgotten?</Title>
+    <hr/>
     <PasswordForgetForm />
   </ContentDiv>
 
@@ -64,15 +133,18 @@ class PasswordForgetForm extends Component {
 
     return (
       <form onSubmit={this.onSubmit}>
-        <input
-          value={this.state.email}
-          onChange={event => this.setState(updateByPropertyName('email', event.target.value))}
-          type="text"
-          placeholder="Email Address"
-        />
-        <button disabled={isInvalid} type="submit">
+        <LineDiv>
+          <IconForm src={email_icon}/>
+          <InputForm
+            value={this.state.email}
+            onChange={event => this.setState(updateByPropertyName('email', event.target.value))}
+            type="text"
+            placeholder="Email Address"
+          />
+        </LineDiv>
+        <ButtonForm disabled={isInvalid} type="submit">
           Reset My Password
-        </button>
+        </ButtonForm>
 
         { error && <p>{error.message}</p> }
       </form>
