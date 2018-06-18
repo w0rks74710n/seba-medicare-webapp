51f43921e83782f60931f63f1605d849dd65ab51
Saner Turhaner <saner.turhaner@tum.de>
2018-06-18 20:35:41 +0200

    Links are added on HomeContent

--- src/containers/Home/HomeContent.js
+++ src/containers/Home/HomeContent.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { FilterSidebar, PrivacyPolicy, TermsAndConditions } from "../";
 import SignIn from "../../components/RegisterationLogin/SignIn";
+import SignUp from "../../components/RegisterationLogin/SignUp";
+import PasswordForget from "../../components/RegisterationLogin/PasswordForget";
 
 const ContentContainer = styled.div`
     width: 100%;
@@ -19,6 +21,8 @@ class HomeContent extends Component {
           <Route exact path={"/"} component={FilterSidebar} />
           <Route path={"/home"} component={FilterSidebar} />
           <Route path={"/register"} component={SignIn} />
+          <Route path={"/sign-up"} component={SignUp} />
+          <Route path={"/password-forget"} component={PasswordForget} />
           <Route path={"/join-us-as-a-doctor"} component={SignIn} />
           <Route path={"/privacy-policy"} component={PrivacyPolicy} />
           <Route path={"/terms-and-conditions"} component={TermsAndConditions} />
