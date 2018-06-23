9b04c2f73001a93a079d0dfd17d87fe5f9b8bce4
Saner Turhaner <saner.turhaner@tum.de>
2018-06-23 15:35:17 +0200

    Schedule of Appointments is kinda list of appoitnments

--- src/components/DashboardSchedule/ScheduleOfAppoinments.js
+++ src/components/DashboardSchedule/ScheduleOfAppoinments.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ColorPalette from '../../constants/ColorPalette'
-import AppointmentList from '../AppointmentList/AppointmentList';
+import AppointmentListItem from './AppointmentListItem';
 
 const Table = styled.table`
   width: 90%;
@@ -92,6 +92,24 @@ const TBodyTRHourSpan = styled.span`
   -webkit-transform:rotate(270deg);
   -moz-transform:rotate(270deg);
 `;
+
+const sampleAppointment1 = {
+  doctorName: 'Dr Saner Turhaner',
+  patientName: 'Roxhens Meraj',
+  illness: 'Depression based on anxiety and discomfort',
+  date: '2018-06-25',
+  time: '11:00',
+  appointment_id: '123456789'
+};
+
+const sampleAppointment2 = {
+  doctorName: 'Dr Saner Turhaner',
+  patientName: 'Sebastian Speth',
+  illness: 'Mind frecking panic attacks',
+  date: '2018-06-25',
+  time: '13:00',
+  appointment_id: '123456780'
+};
 
 class ScheduleOfAppoinments extends Component {
   render() {
