84b6644875c889f82ae6868d0ed0dbeade412e61
Sebastian Speth <speth@in.tum.de>
2018-07-01 00:35:22 +0200

    MapFunctionality added

--- src/components/MapSidebar/MapSidebar.js
+++ src/components/MapSidebar/MapSidebar.js
@@ -1,10 +1,12 @@
+/*global google*/
+
 import React from 'react';
 import styled from 'styled-components';
 import logoTUM from '../../resources/Logo_KlinikumRechtsDerIsar.svg';
 import logoLMU from '../../resources/Logo_LMUKlinikum.png';
 import ColorPalette from "../../constants/ColorPalette";
-import { compose, withProps } from "recompose";
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { compose, withProps, withHandlers, withState, withStateHandlers } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
 
 
 const MapPartnerSidebarContainer = styled.div`
@@ -83,19 +85,51 @@ function Partners(props) {
 
 const MyMapComponent = compose(
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCW0iwTytb3hDtAB_zpfMhUWDmZNDWIEs4&v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `440px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
     }),
     withScriptjs,
-    withGoogleMap
+    withGoogleMap,
+    withState('places', 'updatePlaces', ''),
+    withHandlers(() => {
+        const refs = {
+            map: undefined,
+        }
+
+        return {
+            onMapMounted: () => ref => {
+                refs.map = ref
+            },
+            fetchPlaces: ({ updatePlaces }) => {
+                let places;
+                const bounds = refs.map.getBounds();
+                const service = new google.maps.places.PlacesService(refs.map.context.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
+                const request = {
+                    bounds: bounds,
+                    type: ['doctor']
+                };
+                service.nearbySearch(request, (results, status) => {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        console.log(results);
+                        updatePlaces(results);
+                    }
+                })
+            }
+        }
+    }),
 )((props) =>
     <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        onTilesLoaded={props.fetchPlaces}
+        ref={props.onMapMounted}
+        onBoundsChanged={props.fetchPlaces}
+        defaultZoom={14}
+        defaultCenter={{ lat: 48.1351253, lng: 11.581980499999986 }}
     >
-        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
+        {props.places && props.places.map((place, i) =>
+            <Marker key={i} position={{ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }} />
+        )}
     </GoogleMap>
 )
 
